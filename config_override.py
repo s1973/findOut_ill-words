83881d3733353abef272a53533383ca63840f27b
jobshihainan <whisper1564@gmail.com>
2020-01-17 14:22:42 +0800

    comment sql schema

--- config_override.py
+++ config_override.py
@@ -15,3 +15,28 @@ configs = {
         "password": "17e0ab4a0bdc3871"
     }
 }
+
+'''
+CREATE TABLE `illegal_result` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `keys` mediumtext NOT NULL,
+  `title` text NOT NULL,
+  `postid` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `class_name` varchar(255) NOT NULL,
+  `t_table` char(50) NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `sitename` varchar(255) NOT NULL,
+  `domain` varchar(255) NOT NULL,
+  `en_project` varchar(255) NOT NULL,
+  `siteid` int(11) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8
+
+CREATE TABLE `illegal_words` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `word` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8
+'''
